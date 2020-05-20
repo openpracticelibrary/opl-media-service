e268fc82f22cf9b70abb9c082f14a0f6ec4fd9f0
Shahein Moussavi <shaheinm@gmail.com>
2020-05-20 15:45:07 -0400

    Added dockerfile

--- uploaders/index.js
+++ uploaders/index.js
@@ -3,7 +3,6 @@ const AWSS3Uploader = require('./AWSS3Uploader');
 const GitHubUploader = require('./GitHubUploader');
 
 const s3Uploader = new AWSS3Uploader(AWSConfig);
-console.log(GitHubConfig);
 const gitUploader = new GitHubUploader(GitHubConfig);
 
 module.exports = { gitUploader, s3Uploader };
